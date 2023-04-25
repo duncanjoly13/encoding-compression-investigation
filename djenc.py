f025a11975cf2747aabdb9a58b29e7e9fad4d963
Duncan Joly <duncanjoly13@gmail.com>
2023-04-25 16:48:02 -0500

    began implementation of NaCl encryption

--- djenc.py
+++ djenc.py
@@ -1,8 +1,8 @@
+#TODO finish NaCl class
 #TODO fix extra \n
-#TODO implement AES-16, AES-32 - pycrypto?
-#TODO implement RSA or other method - rsa module
+#TODO implement AES or RSA
 
-import os, hashlib, time
+import os, hashlib, time, nacl.secret, nacl.utils
 from cryptography.fernet import Fernet
 
 class DJFernet:
@@ -52,8 +52,50 @@ class DJFernet:
             f_out.close()
         self.decryptionTime = time.time() - decryptionStartTime
 
+class NaCl:
+    def __init__(self, filename = '2000-word-text.txt', key = ''):
+        self.filesize = os.path.getsize(filename)
+        self.type = 'NaCl'
+        self.filename = filename
+        self.encryptionTime = 0
+        self.decryptionTime = 0
+        self.decryptedSize = 0
+        self.encryptedSize = 0
+        # self.hashString = ''
+        if key == '':
+            self.key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+        else:
+            self.key = key
+        self.safe = nacl.secret.SecretBox(self.key)
+
+    def encrypt(self):
+        encryptionStartTime = time.time()
+        with open(self.filename) as f_in:
+            with open(str(self.filename + '.nacl'), 'wb') as f_out:
+                f_out.write(self.safe.encrypt(f_in.read().encode()))
+                f_out.close()
+                
+                with open(self.filename + '.nacl') as encryptedFile:
+                    with open(str(hashlib.sha1(str(encryptedFile.read()).encode()).hexdigest()) + '-key', 'wb') as keyfile:
+                        # keyfile.write(str(self.safe.__hash__()).encode())
+                        keyfile.write(self.key)
+
+                f_out.close()
+
+            f_in.close()
+        self.encryptionTime = time.time() - encryptionStartTime
+
+    # def decrypt(self):
+
+
 if __name__ == '__main__':
     '''initial = DJFernet('2000-word-text.txt')
     encrypted = initial.encrypt()
     toDecrypt = DJFernet('2000-word-text.txt.frnt')
     decrypted = toDecrypt.decrypt()'''
+
+    initial = NaCl('2000-word-text.txt')
+    encrypted = initial.encrypt()
+    with open('2000-word-text.txt.nacl') as file:
+        toDecrypt = NaCl('2000-word-text.txt.nacl', hashlib.sha1(file.read()).hexdigest())
+        # decrypted = toDecrypt.decrypt()
