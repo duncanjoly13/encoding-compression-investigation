bfb339717921e2f2fa424973eaeba4dcd8a3c11e
Duncan Joly <duncanjoly13@gmail.com>
2023-05-10 22:21:16 -0500

    started work on NaCl

--- djenc.py
+++ djenc.py
@@ -1,4 +1,5 @@
-#TODO implement NaCl, AES or RSA
+#TODO fix NaCl UnicodeDecodeError
+#TODO implement AES or RSA
 
 import os, hashlib, nacl.secret, nacl.utils
 from cryptography.fernet import Fernet
@@ -31,9 +32,48 @@ class DJFernet:
             f_in.close()
         decryptKey = Fernet(self.key)
         return decryptKey.decrypt(self.data)
+    
+class NaCl:
+    def __init__(self, data):
+        self.key = ''
+        self.type = 'NaCl'
+        self.suffix = '.nacl'
+        self.data = data
+
+    def encrypt(self):
+        self.key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+        self.box = nacl.secret.SecretBox(self.key)
+        if type(self.data) != bytes:
+            self.data = str.encode(self.data)
+        encMessage = self.box.encrypt(self.data)
+        with open('./keys/' + encMessage.decode("utf-8")[:10] + '.key', 'wb') as keyFP:
+            keyFP.write(self.key)
+            keyFP.flush()
+            keyFP.close()
+        return encMessage
+
+    def decrypt(self):
+        keyprefix = self.data[:10]
+        if type(self.data) == bytes:
+            keyprefix = self.data[:10].decode()
+        with open('./keys/' + keyprefix + '.key', 'rb') as f_in:
+            self.key = f_in.read()
+            f_in.close()
+        self.box = nacl.secret.SecretBox(self.key)
+        decMessage = self.box.decrypt(self.data)
+        return decMessage
 
 if __name__ == '__main__':
-    initial = DJFernet('2000-word-text.txt')
-    encrypted = initial.encrypt()
-    toDecrypt = DJFernet('2000-word-text.txt.frnt')
-    decrypted = toDecrypt.decrypt()
+    # Test NaCl
+    with open('2000-word-text.txt') as file:
+        testNaCl = NaCl(file.read())
+        with open('encrypted.out', 'wb') as encrypted:
+            encrypted.write(testNaCl.encrypt())
+            encrypted.close()
+        file.close()
+    with open('encrypted.out', 'rb') as toDecrypt:
+        toDecryptNaCl = NaCl(toDecrypt.read())
+        with open('finished.out', 'w') as finalFile:
+            finalFile.write(toDecryptNaCl.decrypt().decode())
+            finalFile.close()
+        toDecrypt.close()
