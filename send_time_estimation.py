d6bddddb6baa84978248a6758da45b33cd0882cd
Duncan Joly <duncanjoly13@gmail.com>
2023-05-19 20:34:53 -0500

    cleanup

--- send_time_estimation.py
+++ send_time_estimation.py
@@ -3,17 +3,17 @@ def get_time(filesize, network_speed):
 
 def edit(filename):
     newData = ''
-    with open(filename) as file:
-        newData += file.readline()[:-1] + ',estimated network time @ 1Mbps (s),estimated network time @ 5Mbps (s),estimated network time @ 10Mbps (s)\n'
-        data = file.readlines()
-        for line in data:
-            speed = int(line.split(',')[7])
-            newData += line[:-1] + ',' + str(get_time(speed, 1)) + ',' + str(get_time(speed, 5)) + ',' + str(get_time(speed, 10)) + '\n'
-        file.close()
-    print(newData)
+    try:
+        with open(filename) as file:
+            newData += file.readline()[:-1] + ',estimated network time @ 1Mbps (s),estimated network time @ 5Mbps (s),estimated network time @ 10Mbps (s)\n'
+            data = file.readlines()
+            for line in data:
+                speed = int(line.split(',')[7])
+                newData += line[:-1] + ',' + str(get_time(speed, 1)) + ',' + str(get_time(speed, 5)) + ',' + str(get_time(speed, 10)) + '\n'
+            file.close()
 
-    with open(filename, 'w') as newFile:
-        newFile.write(newData[:-1])
-        newFile.close()
-
-edit('2023-05-17--22-45-results.csv')
+        with open(filename, 'w') as newFile:
+            newFile.write(newData[:-1])
+            newFile.close()
+    except FileNotFoundError:
+        print("File '%s' does not exist" %filename)
