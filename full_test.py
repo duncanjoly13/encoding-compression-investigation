961c5e3568c8e9e549a28c7f1cf541f0e6a1d3f0
Duncan Joly <duncanjoly13@gmail.com>
2023-06-20 16:25:08 -0500

    added TODO

--- full_test.py
+++ full_test.py
@@ -1,4 +1,5 @@
 #TODO box plot of counts from characterization.py
+#TODO integrate charaacterization into test - source file and after encryption only
 
 #TODO find file size where preferred order switches - between 10 and 95MB - binary search
 #TODO make cleaner table (drop rarely used items)
