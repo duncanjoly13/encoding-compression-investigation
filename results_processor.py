1cd8ae0d3c9d3c2e3a50771521ab0f1d7a6476aa
Duncan Joly <duncanjoly13@gmail.com>
2023-06-29 20:02:24 -0500

    csv generation works, need to fix get_DATA()

--- results_processor.py
+++ results_processor.py
@@ -31,34 +31,51 @@ def get_min(df, file, encAlg, compAlg, order, field):
 def get_unique(df, column):
     return df[column].unique()
 
-def create_csv(df, name):
+def create_csv(df, filename):
     stats = {'mean':get_mean, 'std':get_std, 'max':get_max, 'min':get_min}
-    fields = df.columns.values.tolist()
-    toRemove = ['source file', 'encryption algorithm', 'compression algorithm', 'order', 'repetition #']
-    for notWanted in toRemove:
-        fields.pop(fields.index(notWanted))
-    print(fields)
+    fields = trimmedFields = df.columns.values.tolist()
+    values = {field: 'not yet initialized' for field in fields}
+    toRemove = ['repetition #', 'initial characterization - mean', 'initial characterization - std', 'initial characterization - max', 'initial characterization - total keys', 'after encryption characterization - mean', 'after encryption characterization - std', 'after encryption characterization - max', 'after encryption characterization - total keys']
     for stat in stats.keys():
-        sheet = full_test.Sheet(str(stat.upper() + '-' + name))
+        method = stats[stat]
+        sheet = full_test.Sheet(str(stat.upper() + '-' + filename), ','.join(fields) + '\n')
         for name in list(df['source file'].unique()):
             for encAlg in list(df['encryption algorithm'].unique()):
                 for compAlg in list(df['compression algorithm'].unique()):
                     for order in list(df['order'].unique()):
-                        for field in fields:
-                            method = stats[stat]
-                            sheet.addData(method(df, name, encAlg, compAlg, order, field))
+                        values = values.fromkeys(values, 'BLANK')
+                        headerFields = {'source file':name, 'encryption algorithm':encAlg, 'compression algorithm':compAlg, 'order':order}
+                        for field in trimmedFields:
+                            if field in headerFields.keys():
+                                values[field] = headerFields[field]
+                            elif field in toRemove:
+                                values[field] = 'REMOVED'
+                            else:
+                                datapoint = method(df, name, encAlg, compAlg, order, field)
+                                values[field] = datapoint
+                        i = 0
+                        line = ""
+                        while i < len(values.keys()):
+                            for value in values.keys():
+                                if i == len(values.keys()) - 1:
+                                    line += (str(values[value]) + '\n')
+                                    i += 1
+                                else:
+                                    line += (str(values[value]) + ',')
+                                    i += 1
+                        sheet.addData(line)
 
 if __name__ == '__main__':
     files = glob.glob('*results-PROCESSED.csv')
     for file in files:
         data = pd.read_csv(file)
-        print('mean:', get_mean(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Compression First', 'compression ratio'))
-        print('std:', get_std(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Compression First', 'compression ratio'))
-        print('max:', get_max(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Compression First', 'compression ratio'))
-        print('min:', get_min(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Compression First', 'compression ratio'))
-        print('mean:', get_mean(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Encryption First', 'compression ratio'))
-        print('std:', get_std(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Encryption First', 'compression ratio'))
-        print('max:', get_max(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Encryption First', 'compression ratio'))
-        print('min:', get_min(data, 'single-packet-tele-payload-bytes.data', 'Fernet', 'bzip', 'Encryption First', 'compression ratio'))
+        print('mean:', get_mean(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Compression First', 'compression ratio'))
+        print('std:', get_std(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Compression First', 'compression ratio'))
+        print('max:', get_max(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Compression First', 'compression ratio'))
+        print('min:', get_min(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Compression First', 'compression ratio'))
+        print('mean:', get_mean(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Encryption First', 'compression ratio'))
+        print('std:', get_std(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Encryption First', 'compression ratio'))
+        print('max:', get_max(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Encryption First', 'compression ratio'))
+        print('min:', get_min(data, 'single-packet-tele-payload-bytes.data', 'NoEnc', 'NoZip', 'Encryption First', 'compression ratio'))
 
-        create_csv(data, file)
+        # create_csv(data, file)
