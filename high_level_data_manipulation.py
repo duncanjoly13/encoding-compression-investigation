6d6575ef654c107b1bf3b14fce1cf7e6b51c773d
Duncan Joly <duncanjoly13@gmail.com>
2023-07-06 18:49:07 -0500

    added easier filtering, set to plot by given algs

--- high_level_data_manipulation.py
+++ high_level_data_manipulation.py
@@ -5,6 +5,8 @@ from scipy.stats import ttest_ind
 data_df = pd.read_csv("completed_results\\2023-07-04--00-09_LARGE-32_results-PROCESSED.csv")
 metric = 'total time EXCL. read + write times (ms)'
 desired_filesize = 10239975
+enc_alg = 'NaCl'
+comp_alg = 'gzip'
 
 data_df = data_df[data_df['encryption algorithm'] != 'NoEnc']
 data_df = data_df[data_df['compression algorithm'] != 'NoZip']
@@ -24,7 +26,9 @@ each_unique_filesize.sort()
 
 for filesize in each_unique_filesize:
     print('file: ' + str(filesize) + 'B...')
-    sub_df = data_df[data_df['source file size (B)'] == filesize]
+    first_sub_df = data_df[data_df['source file size (B)'] == filesize]
+    sub_df = first_sub_df[(first_sub_df['compression algorithm'] == comp_alg) & (first_sub_df['encryption algorithm'] == enc_alg)]
+    # sub_df = first_sub_df
     print('compression first mean (ms)', sub_df[sub_df['order'] == 'Compression First'][metric].mean())
     print('encryption first mean (ms)', sub_df[sub_df['order'] == 'Encryption First'][metric].mean())
 
@@ -32,9 +36,12 @@ for filesize in each_unique_filesize:
     print('enc only', str(sub_df[(sub_df['compression algorithm'] == 'NoZip') & (sub_df['encryption algorithm'] != 'NoEnc')][metric].mean()))'''
 
     if filesize == desired_filesize:
-        boxplot = sub_df.boxplot(column = [metric], by = ['operation id'], rot=90, showmeans = True)
-        plt.title(filesize)
-        plt.show()
+        boxplot = sub_df.boxplot(column = [metric], by = ['operation id'], rot=0, showmeans = True, showfliers = False)
+        plt.xlabel('Algorithmic Combination')
+        plt.ylabel('Operation Time (ms)')
+        plt.suptitle('')
+        plt.title('{} and {} in a 10MB File'.format(comp_alg, enc_alg))
+        plt.savefig('best_algs_10mb.png', dpi=300, bbox_inches='tight', pad_inches=.25)
         plt.close()
     test_group_df = pd.DataFrame()
     for id in data_df['operation id'].unique():
@@ -47,10 +54,10 @@ for filesize in each_unique_filesize:
         row['samples'] = [len(second_df)]
         test_group_df = pd.concat([test_group_df, row])
 
-    comp_first_nacl_zip = sub_df[(sub_df['compression algorithm'] == 'zip') & (sub_df['encryption algorithm'] == 'NaCl') & (sub_df['order'] == 'Compression First')]
-    enc_first_nacl_zip = sub_df[(sub_df['compression algorithm'] == 'zip') & (sub_df['encryption algorithm'] == 'NaCl') & (sub_df['order'] == 'Encryption First')]
+    comp_first = sub_df[(sub_df['compression algorithm'] == comp_alg) & (sub_df['encryption algorithm'] == enc_alg) & (sub_df['order'] == 'Compression First')]
+    enc_first = sub_df[(sub_df['compression algorithm'] == comp_alg) & (sub_df['encryption algorithm'] == enc_alg) & (sub_df['order'] == 'Encryption First')]
 
-    test_result = ttest_ind(comp_first_nacl_zip[metric], enc_first_nacl_zip[metric], equal_var = False)
+    test_result = ttest_ind(comp_first[metric], enc_first[metric], equal_var = False)
     print(test_result)
     print(test_result.confidence_interval())
     
