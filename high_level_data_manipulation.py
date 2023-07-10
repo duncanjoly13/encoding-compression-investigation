25a0b84c19d1b424ffebc9c7f4001f2b08db5ce1
Duncan Joly <duncanjoly13@gmail.com>
2023-07-10 11:47:33 -0500

    added graphing code, approach descriptor

--- high_level_data_manipulation.py
+++ high_level_data_manipulation.py
@@ -8,39 +8,55 @@ desired_filesize = 10239975
 enc_alg = 'NaCl'
 comp_alg = 'gzip'
 
-data_df = data_df[data_df['encryption algorithm'] != 'NoEnc']
-data_df = data_df[data_df['compression algorithm'] != 'NoZip']
+'''data_df = data_df[data_df['encryption algorithm'] != 'NoEnc']
+data_df = data_df[data_df['compression algorithm'] != 'NoZip']'''
 
 data_df['operation id'] = data_df['order'] + '-' + data_df['compression algorithm'] + '-' + data_df['encryption algorithm']
+data_df['approach'] = ''
 
 comp_first_sub_df = data_df[data_df['order'] == 'Compression First']
 comp_first_sub_df['operation id'] = comp_first_sub_df['compression algorithm'] + '-then-' + comp_first_sub_df['encryption algorithm']
+comp_first_sub_df['approach'] = 'Compression First'
 
 enc_first_sub_df = data_df[data_df['order'] == 'Encryption First']
 enc_first_sub_df['operation id'] = enc_first_sub_df['encryption algorithm'] + '-then-' +  enc_first_sub_df['compression algorithm']
+enc_first_sub_df['approach'] = 'Encryption First'
 
-data_df = pd.concat([comp_first_sub_df,enc_first_sub_df])
+no_enc_sub_df = data_df[data_df['encryption algorithm'] == 'NoEnc']
+no_enc_sub_df['approach'] = 'Compression Only'
+
+no_comp_sub_df = data_df[data_df['compression algorithm'] == 'NoZip']
+no_comp_sub_df['approach'] = 'Encryption Only'
+
+data_df = pd.concat([comp_first_sub_df,enc_first_sub_df, no_enc_sub_df, no_comp_sub_df])
 
 each_unique_filesize = data_df['source file size (B)'].unique()
 each_unique_filesize.sort()
 
 for filesize in each_unique_filesize:
     print('file: ' + str(filesize) + 'B...')
-    first_sub_df = data_df[data_df['source file size (B)'] == filesize]
-    sub_df = first_sub_df[(first_sub_df['compression algorithm'] == comp_alg) & (first_sub_df['encryption algorithm'] == enc_alg)]
-    # sub_df = first_sub_df
+    
+    correct_filesize_df = data_df[data_df['source file size (B)'] == filesize]
+
+    # FOR SPECIFIC ALGS AND THEIR BASELINES
+    '''correct_algs_df = correct_filesize_df[(correct_filesize_df['compression algorithm'] == comp_alg) & (correct_filesize_df['encryption algorithm'] == enc_alg)]
+    no_enc_df = correct_filesize_df[(correct_filesize_df['compression algorithm'] == comp_alg) & (correct_filesize_df['encryption algorithm'] == 'NoEnc') & (correct_filesize_df['order'] == 'Compression First')]
+    no_comp_df = correct_filesize_df[(correct_filesize_df['compression algorithm'] == 'NoZip') & (correct_filesize_df['encryption algorithm'] == enc_alg) & (correct_filesize_df['order'] == 'Compression First')]
+    sub_df = pd.concat([correct_algs_df, no_enc_df, no_comp_df])'''
+
+    # FOR AVERAGES AND BASELINES (COMP FIRST, ENC FIRST, COMP ONLY, ENC ONLY)
+    sub_df = correct_filesize_df
+        
     print('compression first mean (ms)', sub_df[sub_df['order'] == 'Compression First'][metric].mean())
     print('encryption first mean (ms)', sub_df[sub_df['order'] == 'Encryption First'][metric].mean())
 
-    '''print('comp only', str(sub_df[(sub_df['compression algorithm'] != 'NoZip') & (sub_df['encryption algorithm'] == 'NoEnc')][metric].mean()))
-    print('enc only', str(sub_df[(sub_df['compression algorithm'] == 'NoZip') & (sub_df['encryption algorithm'] != 'NoEnc')][metric].mean()))'''
-
     if filesize == desired_filesize:
-        boxplot = sub_df.boxplot(column = [metric], by = ['operation id'], rot=0, showmeans = True, showfliers = False)
+        # by = ['order'] for most, by = ['approach'] for high-level baselines
+        boxplot = sub_df.boxplot(column = [metric], by = ['approach'], rot = 45, showmeans = True, showfliers = False)
         plt.xlabel('Algorithmic Combination')
         plt.ylabel('Operation Time (ms)')
         plt.suptitle('')
-        plt.title('{} and {} in a 10MB File'.format(comp_alg, enc_alg))
+        plt.title('Average Operation Times in a 10MB File'.format(comp_alg, enc_alg))
         plt.savefig('RENAME_THIS.png', dpi=300, bbox_inches='tight', pad_inches=.25)
         plt.close()
     test_group_df = pd.DataFrame()
